f644735a10fdb8bb8a23b990886a266e4d089471
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-03-13 11:16:45 -0400

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,12 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     },
+  
+  pool: {
+    afterCreate: (conn, done) => {
+        conn.res('PRAGMA foreign_keys = ON', done);
+        } 
+    },
   },
+
 };
